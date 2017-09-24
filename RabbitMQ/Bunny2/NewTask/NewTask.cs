6af24de90be79a4d19c5b9e85037b783a33e8aaf
oles <o.lepetya@gmail.com>
2017-09-25 00:44:02 +0300

    Для генерації нових завдань зроблено розбивання вхідного повідомлення через  semicolon(;)  щоб було простіше тестувати.

--- RabbitMQ/Bunny2/NewTask/NewTask.cs
+++ RabbitMQ/Bunny2/NewTask/NewTask.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using RabbitMQ.Client;
 using System.Text;
+using System.Linq;
+using System.Collections.Generic;
 
 class NewTask
 {
@@ -10,24 +12,23 @@ class NewTask
         using(var connection = factory.CreateConnection())
         using(var channel = connection.CreateModel())
         {
-            channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
-
-            var message = GetMessage(args);
-            var body = Encoding.UTF8.GetBytes(message);
-
-            var properties = channel.CreateBasicProperties();
-            properties.Persistent = true;
-
-            channel.BasicPublish(exchange: "", routingKey: "task_queue", basicProperties: properties, body: body);
-            Console.WriteLine(" [x] Sent {0}", message);
+            channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null );
+            var messages = GetMessage(args);
+            foreach(var message in messages){
+                var body = Encoding.UTF8.GetBytes(message);
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+                channel.BasicPublish(exchange: "", routingKey: "task_queue", basicProperties: properties, body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }            
         }
 
         Console.WriteLine(" Press [enter] to exit.");
         Console.ReadLine();
     }
 
-    private static string GetMessage(string[] args)
+    private static IEnumerable<string> GetMessage(string[] args)
     {
-        return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        return ((args.Length > 0) ? args.SelectMany(one=>one.Split(";")) :new System.Collections.Generic.List<string>{"Hello World!"});
     }
 }
